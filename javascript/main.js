51a49e3858ac35dde3a5068ef98865f0f400c425
followynne <matteo.gregoricchio@libero.it>
2019-07-29 23:35:31 +0200

    end day - completed portfolio, about, contacts - corrected wrong import for libraries (script at the end of the body wasn't getting jquery and bootstrap) - started carousel work

--- javascript/main.js
+++ javascript/main.js
@@ -1,5 +1,22 @@
 import $ from 'jquery'
+import 'popper.js'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.bundle'
+import 'bootstrap/dist/js/bootstrap'
 
-console.log("Ciao Matteo");
+$(document).ready(()=>{
+    $('.scroll').on('click', (e) =>  {
+        e.preventDefault();
+        let link = $(event.target).attr('href');
+        if ($(event.target).is('img')){
+            let node = $(event.target).parentsUntil();
+            node.each((index) => {            
+                if ($(node[index]).is('a')){
+                        link = $(node[index]).attr('href');
+                    }
+            });
+        }
+        $('html, body').animate({
+            scrollTop: $(link).offset().top
+        }, 'slow');
+    })
+})
