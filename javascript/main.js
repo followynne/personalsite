f21e87cc14efe240f6108f680d061dc1fd28ca98
Matteo Gregoricchio <matteo.gregoricchio@libero.it>
2021-01-24 22:04:50 +0100

    re-styled site, made it Eng first

--- javascript/main.js
+++ javascript/main.js
@@ -37,24 +37,21 @@ const printDate = () => {
 
   $('#itsCourse').popover({
     content:
-      '.NET Framework/C#, 106hr' +
+      '.NET Framework / C#, 106hrs' +
       '</br>PHP PROGRAMMING, 106hr' +
-      '</br>FONDAMENTI DI PROGRAMMAZIONE, 75hr' +
-      '</br>PROGRAMMAZIONE IN JAVA, 70hr' +
-      '</br>PROGRAMMAZIONE WEB-FRONT-END(HTML, CSS, Javascript), 70hr' +
+      '</br>Programming Fundamentals, 75hr' +
+      '</br>JAVA Development, 70hr' +
+      '</br>WEB-FRONT-END Development (HTML, CSS, Javascript), 70hr' +
       '</br>CLOUD SERVICES, 70hr' +
-      '</br>FONDAMENTI DI BASI DI DATI, 60hr' +
+      '</br>Databases Fundamentals, 60hr' +
       '</br>Microsoft SQL Server, 68hr' +
-      '</br>BASI DI DATI NON RELAZIONALI (NOSQL), 25hr' +
-      '</br>FONDAMENTI DI DATA WAREHOUSING E DATA ANALYTICS, 20hr' +
-      '</br>ARCHITETTURE SOFTWARE PER MOBILE E WEB, 18hr' +
-      '</br>RETI DI CALCOLATORI, 20hr' +
-      '</br>SICUREZZA INFORMATICA, 20hr' +
-      '</br>SVILUPPO BACK-END IN NODE.JS, 58hr' +
-      '</br>INTERACTION DESIGN, VISUAL DESIGN E PROGETTAZIONE CONCETTUALE, 44hr' +
-      '</br>PIATTAFORMA E-COMMERCE MAGENTO, 75hr' +
-      '</br>FONDAMENTI DI PROGRAMMAZIONE DI APPLICAZIONI MOBILE (ANDROID/IOS), 60hr' +
-      '</br>METODOLOGIE E STRUMENTI DI PROJECT MANAGEMENT, 30hr' +
-      '</br>COPYRIGHT E NORME GIURIDICHE DEL MONDO DIGITALE, 20hr',
+      '</br>Non-relational Databases Fundamentals (on MongoDB), 25hr' +
+      '</br>DATA WAREHOUSING & DATA ANALYTICS Basics, 20hr' +
+      '</br>Mobile & Web Software architecture, 18hr' +
+      '</br>Computer Networks, 20hr' +
+      '</br>Cyber Security, 20hr' +
+      '</br>BACK-END development with Node.Js, 58hr' +
+      '</br>INTERACTION DESIGN, VISUAL DESIGN Basics, 44hr' +
+      '</br>PROJECT MANAGEMENT Basics, 30hr',
   });
 })();
