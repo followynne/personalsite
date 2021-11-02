7b3806810928817fd6bd9b12e8eda6304a8f482d
Matteo Gregoricchio <matteo.gregoricchio@libero.it>
2021-11-02 09:05:09 +0100

    perf(main): make popover import as conditional dynamic module

--- javascript/main.ts
+++ javascript/main.ts
@@ -1,11 +1,9 @@
-import { Popover } from 'bootstrap';
-
 const printDate = () => {
   const date = new Date().getFullYear();
   document.getElementById('currentYearPrint').append(date.toString());
 };
 
-(() => {
+(async () => {
   document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
     anchor.addEventListener('click', function (e) {
       e.preventDefault();
@@ -18,23 +16,27 @@ const printDate = () => {
 
   printDate();
 
-  new Popover(document.getElementById('itsCourse'), {
-    content:
-      '.NET Framework / C#, 106hrs' +
-      '</br>PHP PROGRAMMING, 106hr' +
-      '</br>Programming Fundamentals, 75hr' +
-      '</br>JAVA Development, 70hr' +
-      '</br>WEB-FRONT-END Development (HTML, CSS, Javascript), 70hr' +
-      '</br>CLOUD SERVICES, 70hr' +
-      '</br>Databases Fundamentals, 60hr' +
-      '</br>Microsoft SQL Server, 68hr' +
-      '</br>Non-relational Databases Fundamentals (on MongoDB), 25hr' +
-      '</br>DATA WAREHOUSING & DATA ANALYTICS Basics, 20hr' +
-      '</br>Mobile & Web Software architecture, 18hr' +
-      '</br>Computer Networks, 20hr' +
-      '</br>Cyber Security, 20hr' +
-      '</br>BACK-END development with Node.Js, 58hr' +
-      '</br>INTERACTION DESIGN, VISUAL DESIGN Basics, 44hr' +
-      '</br>PROJECT MANAGEMENT Basics, 30hr',
-  });
+  const tooltipItem = document.getElementById('itsCourse');
+  if (tooltipItem) {
+    const { Popover } = await import('bootstrap');
+    new Popover(tooltipItem, {
+      content:
+        '.NET Framework / C#, 106hrs' +
+        '</br>PHP PROGRAMMING, 106hr' +
+        '</br>Programming Fundamentals, 75hr' +
+        '</br>JAVA Development, 70hr' +
+        '</br>WEB-FRONT-END Development (HTML, CSS, Javascript), 70hr' +
+        '</br>CLOUD SERVICES, 70hr' +
+        '</br>Databases Fundamentals, 60hr' +
+        '</br>Microsoft SQL Server, 68hr' +
+        '</br>Non-relational Databases Fundamentals (on MongoDB), 25hr' +
+        '</br>DATA WAREHOUSING & DATA ANALYTICS Basics, 20hr' +
+        '</br>Mobile & Web Software architecture, 18hr' +
+        '</br>Computer Networks, 20hr' +
+        '</br>Cyber Security, 20hr' +
+        '</br>BACK-END development with Node.Js, 58hr' +
+        '</br>INTERACTION DESIGN, VISUAL DESIGN Basics, 44hr' +
+        '</br>PROJECT MANAGEMENT Basics, 30hr',
+    });
+  }
 })();
