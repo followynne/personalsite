280efb960e0ae2d8f83c3cb38af00012512bd32d
Matteo Gregoricchio <matteo.gregoricchio@libero.it>
2021-11-03 13:42:16 +0100

    wip(swiper): styling

--- javascript/swiper.ts
+++ javascript/swiper.ts
@@ -1,35 +1,39 @@
 import Swiper, { Navigation, Pagination } from 'swiper';
 // import Swiper and modules styles
 import 'swiper/swiper-bundle.min.css';
-import 'swiper/';
-// import 'swiper/css/navigation';
-// import 'swiper/css/pagination';
+import '../css/swiper.scss';
+import '@fortawesome/fontawesome-free/css/solid.min.css';
+import '@fortawesome/fontawesome-free/css/fontawesome.min.css';
 
 // configure Swiper to use modules
 Swiper.use([Navigation, Pagination]);
 
 // init Swiper:
-const swiper = new Swiper('.swiper', {
+new Swiper('.swiper', {
   // Optional parameters
   autoplay: true,
+  breakpoints: {
+    500: {
+      slidesPerView: 1,
+    },
+    700: {
+      slidesPerView: 1.5,
+    },
+  },
+  centeredSlides: true,
   direction: 'horizontal',
+  freeMode: true,
+  grabCursor: true,
   lazy: true,
   loop: true,
-
-  // If we need pagination
   pagination: {
     el: '.swiper-pagination',
-    type: 'fraction'
-  },    
-
-  // Navigation arrows
+    clickable: true,
+  },
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
+  slidesPerView: 1.5,
+  spaceBetween: 10,
 });
