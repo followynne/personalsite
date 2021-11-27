15145c5df3af85d74fc46c7f37ef13e4f7f4b4f9
Matteo Gregoricchio <matteo.gregoricchio@libero.it>
2021-11-27 11:33:57 +0100

    Updates

--- articles/github-pages-hosting-with-parcel.bbc67c13.js
+++ articles/github-pages-hosting-with-parcel.bbc67c13.js
@@ -1,2 +1,2 @@
 const e=()=>{const e=document.querySelector(".articles-progress-bar .progress-bar"),o=window.scrollY/(document.body.scrollHeight-window.innerHeight)*100,r=o.toFixed(0);e.style.setProperty("width",`${r}%`),e.ariaValueNow=`${r}%`,e.textContent=o<95?`${r}%`:"Thanks for reading!"};e(),window.addEventListener("scroll",e);
-//# sourceMappingURL=github-pages-hosting-with-parcel.4b10f86b.js.map
+//# sourceMappingURL=github-pages-hosting-with-parcel.bbc67c13.js.map
