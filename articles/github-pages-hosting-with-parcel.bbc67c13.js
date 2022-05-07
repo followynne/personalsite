9fba59e2d6d426a44a13363faec0304cca4ee1d0
Matteo Gregoricchio <matteo.gregoricchio@libero.it>
2022-05-08 00:03:48 +0200

    Updates

--- articles/github-pages-hosting-with-parcel.bbc67c13.js
+++
@@ -1,2 +0,0 @@
-const e=()=>{const e=document.querySelector(".articles-progress-bar .progress-bar"),o=window.scrollY/(document.body.scrollHeight-window.innerHeight)*100,r=o.toFixed(0);e.style.setProperty("width",`${r}%`),e.ariaValueNow=`${r}%`,e.textContent=o<95?`${r}%`:"Thanks for reading!"};e(),window.addEventListener("scroll",e);
-//# sourceMappingURL=github-pages-hosting-with-parcel.bbc67c13.js.map
